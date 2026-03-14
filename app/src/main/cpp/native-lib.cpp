9b3576404e63bd2031591ca5fb84592bfa41f9c7
kmm3344 <kmm3344@github.com>
2026-03-14 13:38:42 +0800

    全新 ImGui Android 项目

---
+++ app/src/main/cpp/native-lib.cpp
@@ -0,0 +1,69 @@
+#include <android/native_activity.h>
+#include <android/log.h>
+#include <EGL/egl.h>
+#include <GLES3/gl3.h>
+
+#include "imgui.h"
+#include "backends/imgui_impl_android.h"
+#include "backends/imgui_impl_opengl3.h"
+
+#define LOGI(...) __android_log_print(ANDROID_LOG_INFO, "ImGui", __VA_ARGS__)
+
+void android_main(struct android_app* app) {
+    app->onAppCmd = [](struct android_app* app, int32_t cmd) {
+        if (cmd == APP_CMD_INIT_WINDOW) {
+            IMGUI_CHECKVERSION();
+            ImGui::CreateContext();
+            ImGuiIO& io = ImGui::GetIO();
+            io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
+
+            ImGui::StyleColorsDark();
+
+            ImGui_ImplAndroid_Init(app->window);
+            ImGui_ImplOpenGL3_Init("#version 300 es");
+
+            bool running = true;
+            while (running) {
+                int events;
+                struct android_poll_source* source;
+                while (ALooper_pollAll(0, NULL, &events, (void**)&source) >= 0) {
+                    if (source) source->process(app, source);
+                }
+
+                ImGui_ImplOpenGL3_NewFrame();
+                ImGui_ImplAndroid_NewFrame();
+                ImGui::NewFrame();
+
+                ImGui::SetNextWindowPos(ImVec2(50, 50), ImGuiCond_FirstUseEver);
+                ImGui::SetNextWindowSize(ImVec2(400, 300), ImGuiCond_FirstUseEver);
+                ImGui::Begin("系统监控", nullptr);
+                ImGui::Text("CPU: 45.6%%");
+                ImGui::ProgressBar(0.456f, ImVec2(-1, 0), "");
+                ImGui::Text("内存: 3.2/8.0 GB");
+                ImGui::ProgressBar(0.4f, ImVec2(-1, 0), "");
+                if (ImGui::Button("关闭")) {
+                    running = false;
+                }
+                ImGui::End();
+
+                ImGui::Render();
+                ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+                eglSwapBuffers(eglGetCurrentDisplay(), eglGetCurrentSurface(EGL_DRAW));
+                usleep(16000);
+            }
+
+            ImGui_ImplOpenGL3_Shutdown();
+            ImGui_ImplAndroid_Shutdown();
+            ImGui::DestroyContext();
+        }
+    };
+
+    while (1) {
+        int events;
+        struct android_poll_source* source;
+        while (ALooper_pollAll(-1, NULL, &events, (void**)&source) >= 0) {
+            if (source) source->process(app, source);
+        }
+    }
+}
